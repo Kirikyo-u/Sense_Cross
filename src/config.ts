3c43615dd139eb31f11f68c875a1ce442c436447
Resage <patchiumenergy@outlook.com>
2025-10-05 21:05:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,6 +54,7 @@ export const siteConfig: SiteConfig = {
 				"/assets/desktop-banner/d6.webp",
 				"/assets/desktop-banner/d7.webp",
 				"/assets/desktop-banner/d8.webp",
+				"/assets/desktop-banner/d9.webp",
 			], // 桌面横幅图片
 			mobile: [
 				"/assets/mobile-banner/m1.webp",
